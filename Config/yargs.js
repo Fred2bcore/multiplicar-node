c0d7e2ce2c9cee49ed1c05b63e7270d0eea2c428
Fredy <fredy.solis@2bcore.com>
2019-04-25 08:12:09 -0500

    comit inicial

--- Config/yargs.js
+++ Config/yargs.js
@@ -1,14 +1,13 @@
 const opts = {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        }
+    base: {
+        demand: true,
+        alias: 'b'
+    },
+    limite: {
+        alias: 'l',
+        default: 10
     }
-    //ijhtgtrtdh
+}
 const argv = require('yargs')
     .command('listar', 'Imprime en Consola la Tabla de Multiplicar', opts)
     .command('crear', 'Genera un archivo con la tabla de muliplicar', opts)
